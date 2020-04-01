fb1367fc37ba586720102f7b3bf2e7ef1984b77c
Melver Dsouza <melverken10@gmail.com>
2020-04-01 18:47:21 +0530

    sound and animation working

--- code/code.js
+++ code/code.js
@@ -1,11 +1,20 @@
 let currentBeat = "65";
 
 $(document).keydown(function(event) {
-  // document.getElementById(event.keyCode).pause();
-  $(`#${currentBeat}`)[0].pause();
-  // document.getElementById(event.keyCode).currentTime = 0;
-  $(`#${currentBeat}`)[0].currentTime = 0;
-  $(`#${event.keyCode}`)[0].play();
-  currentBeat = event.keyCode;
-  console.log(event.keyCode);
+  let audio = $("body").find(`audio[id="${event.keyCode}"]`);
+  audio[0].currentTime = 0;
+  audio[0].play();
+  if (!audio) return;
+  let key = $("body").find(`div[data-key="${event.keyCode}"]`);
+  key.addClass("playing");
+  key.css("border", "3px solid gold");
+  console.log(key);
+  setTimeout(function() {
+    key.removeClass("playing");
+  }, 70);
+  setTimeout(function() {
+    key.css("border", "2px solid gray");
+  }, 80);
 });
+
+function removeClass() {}
